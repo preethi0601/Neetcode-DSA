31ddba22b7884cd2e64d06158a6bbf001b508974
Prithi <48240941+preethi0601@users.noreply.github.com>
2024-07-31 20:02:00 +0200

    Time: 68 ms (66.44%) | Memory: 54.3 MB (20.50%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,5 +4,21 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.split('').sort().join('') === t.split('').sort().join('') ? true : false
+    if(s.length !== t.length) return false
+    // return s.split('').sort().join('') === t.split('').sort().join('') ? true : false;  
+    const hash = {}
+
+    for(let val of s) {
+        hash[val] = (hash[val] || 0) + 1;
+    }
+    console.log(hash);
+
+    for(let i =0;i<t.length;i++) {
+       if(!(t[i] in hash) || hash[t[i]] === 0) {
+           return false;
+       } else {
+           hash[t[i]] -= 1;
+       }
+    }
+    return true;
 };
