1c77f33375d01c241c75ad5bf3228bae7c2fb2d7
Prithi <48240941+preethi0601@users.noreply.github.com>
2024-07-31 19:08:47 +0200

    Time: 66 ms (55.45%) | Memory: 50.5 MB (51.95%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,12 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    if(nums.length < 2) return [];
-    const hash = {};
-    for (let i = 0; i < nums.length; i++) {
-    if (target-nums[i] in hash) {
-      return [hash[target-nums[i]], i];
+   let result = {}
+   for(let i=0;i<nums.length;i++) {
+    if(target - nums[i] in result) {
+        return [i, result[target-nums[i]]] 
     }
-        hash[nums[i]] = i;
-    }
+    result[nums[i]] = i;
+   }
 };
