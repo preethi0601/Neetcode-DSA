0ba228cf36847a7db69f6a6cef19b4756b0112c3
Prithi <48240941+preethi0601@users.noreply.github.com>
2024-08-07 11:22:19 +0200

    Time: 73 ms (20.37%) | Memory: 52.7 MB (15.20%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -3,9 +3,5 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let ans = new Array(nums.length*2);
-    for (let i=0;i<nums.length;i++) {
-        ans[i] = ans[i+nums.length] = nums[i]
-    }
-    return ans
+    return [...nums, ...nums]
 };
