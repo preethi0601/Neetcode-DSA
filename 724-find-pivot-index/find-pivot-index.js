3362b46ac4dd13e20ea9bc9fe6194ada23e3403d
Prithi <48240941+preethi0601@users.noreply.github.com>
2024-09-22 16:38:46 +0200

    Time: 64 ms (68.11%) | Memory: 51.7 MB (77.78%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+    let total = nums.reduce((a, b) => a + b, 0)
+    let left = 0;
+
+    for(let i=0;i<nums.length;i++) {
+        let right = total - left - nums[i]
+
+        if(left === right) return i
+
+        left+=nums[i]
+    }
+    return -1
+};
