958fe22f2420496f73254cc16d8b0045209617e7
Prithi <48240941+preethi0601@users.noreply.github.com>
2024-10-07 17:11:17 +0200

    Time: 73 ms (9.92%) | Memory: 50.8 MB (67.93%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeGood = function(s) {
+    let stack = []
+    for(let ch of s) {
+        if(stack.length > 0 && Math.abs(stack[stack.length-1].charCodeAt(0) - ch.charCodeAt(0)) === 32) {
+            stack.pop()
+        } else stack.push(ch)
+    }
+    return stack.join('')
+};
